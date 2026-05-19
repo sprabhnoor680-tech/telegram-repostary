795b49b0b69384987f38963f77bd5a5456da4c8b
sprabhnoor680-tech <sprabhnoor680@gmail.com>
2026-05-19 13:42:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram.ext import (
     MessageHandler,
     ContextTypes,
     filters,
-)
+)import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN environment variable is not set")
